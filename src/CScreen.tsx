56d94ff0a251a844deda9602bf1dfb660c4981f5
campbellman <cc13.engineering@gmail.com>
2024-02-01 16:20:08 -0500

    done

--- src/CScreen.tsx
+++ src/CScreen.tsx
@@ -177,11 +177,11 @@ export default function CScreen() {
     <>
       <main className="flex flex-col w-full gap-4 sm:gap-8">
         <div className="flex flex-col w-full gap-4 p-4 pb-0 sm:gap-8 sm:p-0">
-          <section className="p-6 mt-0 bg-white rounded-lg shadow-md container-width sm:mt-8 sm:p-8">
+          <section className="p-4 mt-0 bg-white rounded-lg shadow-md container-width sm:mt-8 sm:p-8">
             <CExplanation />
           </section>
           <section
-            className="gap-4 p-6 bg-gray-300 rounded-lg shadow-md container-width sm:gap-8 sm:p-8"
+            className="gap-4 p-4 bg-gray-300 rounded-lg shadow-md container-width sm:gap-8 sm:p-8"
             style={{
               width: "100%",
               display: "flex",
